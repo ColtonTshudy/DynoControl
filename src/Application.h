2cf533906625c72b799658ce2edc77a35ab45848
ColtonTshudy <82547877+ColtonTshudy@users.noreply.github.com>
2022-10-12 19:59:17 -0400

    Fixed formatting, added comments, and versions

--- src/Application.h
+++ src/Application.h
@@ -1,13 +1,19 @@
-/*
- * Application.h
+/**
+ * @file Application.h
  *
- *  Created on: 4/13/2022 Author: Colton Tshduy
+ * @brief Header containing macros, variables, and function initializations for
+ * main.cpp
+ *
+ * @ingroup default
+ *
+ * @author Colton Tshudy [please add your names here!]
+ *
+ * @version 10/12/2022
  */
 
 /* Arduino Includes */
 #include <Arduino.h>
 #include <X9C10X.h>
-
 
 /* HAL Includes */
 #include <HAL.h>
@@ -18,17 +24,16 @@
 
 /* Macros */
 #define US_IN_SECONDS 1000000 // microseconds in a second
-#define POT_MAX_R 96000 // maximum resistance of the X9C104 potentiometer
+#define POT_MAX_R 96000       // maximum resistance of the X9C104 potentiometer
 
+/** =================================================
+ * Primary struct for the application
+ */
 struct _Application
 {
-    // Holds the variables used in main.cpp
-    // =========================================================================
-    // Timers
+    // Variables used in main.cpp Timers
     SWTimer watchdog_timer;
     SWTimer pot_test_timer;
-
-    // Potentiometer
 };
 typedef struct _Application Application;
 
--- src/HAL.h
+++ src/HAL.h
@@ -1,9 +1,14 @@
 /**
- * HAL.h
+ * @file HAL.h
  *
- *  Created on: 4/13/2022
- *      Author: Colton Tshduy
- */     
+ * @brief Header containing macros for hardware components
+ *
+ * @ingroup default
+ *
+ * @author Colton Tshudy [please add your names here!]
+ *
+ * @version 10/12/2022
+ */
 
 /* Arduino Includes */
 #include <Arduino.h>
@@ -11,13 +16,11 @@
 #ifndef HAL_H_
 #define HAL_H_
 
-// --Pin Macros--
-// Pins for LEDs
+// --Pin Macros-- Pins for LEDs
 #define LED_PIN 13
 // Pins for 100k Potentiometer
 #define CS_PIN 4
 #define INC_PIN 3
 #define UD_PIN 2
-
 
 #endif /* HAL_H_ */
